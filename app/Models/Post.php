15fba33dded63309a05b12788608b4dbe7e09b89
Paul Lourence Calicoy <paullourencecalicoy@gmail.com>
2025-08-13 14:58:53 +0800

    Adding Authentication

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,4 +16,9 @@ class Post extends Model
         'title'
     ];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create()
         Post::factory(100)->create();
+        User::factory(100)->create();
     }
 }
