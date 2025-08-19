72a9ecea4131d8d9d2c00fa6944912dfd75508a5
Paul Lourence Calicoy <paullourencecalicoy@gmail.com>
2025-08-19 15:38:54 +0800

    update

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\User;
-use Illuminate\Container\Attributes\Auth;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
